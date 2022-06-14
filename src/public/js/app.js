6e1dd8c717d52dbb8b1e2063ce715ddbf4d7ae8d
cororom <cororom@gmail.com>
2022-06-14 23:30:23 +0900

    Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function handleOpen() {
   console.log("Connected to Server ✅");
-});
+}
+
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
   console.log("New message: ", message.data);
@@ -15,3 +19,12 @@ socket.addEventListener("close", () => {
 setTimeout(() => {
   socket.send("hello from the browser!");
 }, 10000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -15,13 +15,19 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("close", onSocketClose);
   socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
