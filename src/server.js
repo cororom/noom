4136ced7601a19250b7dd286d6287dc8bfa6c4af
cororom <cororom@gmail.com>
2022-06-15 23:43:32 +0900

    SocketIo Using

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,31 +12,16 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIo(httpServer);
 
-function onSocketClose() {
-  console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("Connected to Browser ✅");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload.toString("utf8")}`));
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    setTimeout(() => {
+      done("hello from the backend");
+    }, 15000);
   });
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
