ce1dc59cafc7d1c3c149e028450f8090ce33c167
cororom <cororom@gmail.com>
2022-06-17 23:21:14 +0900

    User Count

--- src/server.js
+++ src/server.js
@@ -30,6 +30,10 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
   wsServer.sockets.emit("room_change", publicRooms());
   socket["nickname"] = "Anon";
@@ -40,11 +44,11 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     socket["nickname"] = nickName;
     done();
-    socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
   });
   socket.on("disconnect", () => {
     wsServer.sockets.emit("room_change", publicRooms());
